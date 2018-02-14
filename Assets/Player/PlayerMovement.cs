cf8ef46357411725567b13bc7bdc67411ef92873
Adam Delo <deloa6133@my.uwstout.edu>
2018-02-14 11:32:28 -0600

    Changed player movement completely
    scrapped the old player movement and created a new type of movement on a different set of physics and collisions so that it is easier in the future to update locations.

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,49 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    // Movement
+    //How fast the player moves (sneaking)
+    public float topSpeed = 5f;
 
-    public float speed;
-    public float jump;
-    float moveVelocity;
+    //determine sprite direction
+    bool faceRight = true;
 
-    //grounded variable (Checks if the player is on the groud for jumping)
-    bool grounded = true;
-	
-	// Update is called once per frame
-	void Update () {
-		//Player Jump (space to jump)
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (grounded == true)
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
-            }
-        }
+    //checks if player is grounded (if player is grounded, can jump, if not, can not)
+    bool grounded = false;//not grounded
+    public Transform groundCheck;//transform at player feet to check if grounded or not
+    float groundRadius = 0.2f;//how large the circle is when checking distance to ground
+    public float jumpForce = 300f;//how high the player can jump
+    public LayerMask whatIsGround;//checks which layer is currently considered the ground
 
-        //reset move speed to 0
-        moveVelocity = 0;
-
-        //left and right player movement
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveVelocity = -speed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveVelocity = speed;
-        }
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-	}
-
-    //check if player is grounded (can jump)
-    private void OnTriggerEnter2D(Collider2D collision)
+    //physics in fixed update
+    private void FixedUpdate()
     {
-        grounded = true;
+        //true or false output for whether the ground transform hit the whatIsGround with the groundRadius
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        //get movement direction of player
+        float move = Input.GetAxis("Horizontal");
+        //add velocity to the direction of player
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        //if facing negative direction and not facing the right FLIP
+        if (move > 0 && !faceRight)
+        {
+            Flip();
+        }else if (move < 0 && faceRight)//re-flips the sprite to the original direction if going back
+        {
+            Flip();
+        }
     }
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void Update()
     {
-        grounded = false;
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            //adding jumpforce to the y-axis of the rigidBody
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    //flips the direction of the sprite depending on which way the player is facing/moving
+    void Flip()
+    {
+        //face the opposite direction
+        faceRight = !faceRight;
+        //get local scale
+        Vector3 theScale = transform.localScale;
+        //flip the sprite on the x axis
+        theScale.x *= -1;
+        //apply the flip to the local scale of the player
+        transform.localScale = theScale;
     }
 }
