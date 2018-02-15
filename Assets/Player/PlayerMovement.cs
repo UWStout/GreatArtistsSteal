adb81f431d5e38bb7848da79a09b7863c4e6578f
deloa6133 <36006013+deloa6133@users.noreply.github.com>
2018-02-14 18:08:03 -0600

    Created wall check
    Unable to currently execute the wall check, player input does not work properly (commented out the rest of the code that i created because it isnt working properly)

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -17,22 +17,35 @@ public class PlayerMovement : MonoBehaviour {
     public float jumpForce = 300f;//how high the player can jump
     public LayerMask whatIsGround;//checks which layer is currently considered the ground
 
+    //checks if player is next to wall (if player is next to wall disable horizontal input (x-axis))
+    bool wallNear = false;//not next to a wall on right side
+    public Transform wallCheck;//transform at player sides to check if they are close to a wall
+    float wallRadius = 0.5f;//how large the circle is when checking distance to wall
+    public LayerMask whatIsWall;//checks which layer is currently considered a wall
+
     //physics in fixed update
     private void FixedUpdate()
     {
+        //true or false output for whether the wall transform hit the whatIsWall with the wallRadius
+        wallNear = Physics2D.OverlapCircle(wallCheck.position, wallRadius, whatIsWall);
+
         //true or false output for whether the ground transform hit the whatIsGround with the groundRadius
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
         //get movement direction of player
         float move = Input.GetAxis("Horizontal");
         //create movement in the player horizontal direction
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        //if (wallCheck && (Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.D)))
+        //{
+            GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        //}
 
         //if facing negative direction and not facing the right FLIP
         if (move > 0 && !faceRight)
         {
             Flip();
-        }else if (move < 0 && faceRight)//re-flips the sprite to the original direction if going back
+        }
+        else if (move < 0 && faceRight)//re-flips the sprite to the original direction if going back
         {
             Flip();
         }
@@ -54,6 +67,8 @@ public class PlayerMovement : MonoBehaviour {
         {
             topSpeed = 5f;
         }
+
+        
     }
 
     //flips the direction of the sprite depending on which way the player is facing/moving
