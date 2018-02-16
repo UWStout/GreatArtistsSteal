f2cb2999daff1820948de405993d10a86f829dbd
deloa6133 <36006013+deloa6133@users.noreply.github.com>
2018-02-16 14:07:49 -0600

    Player jump animation/no stick to walls
    created the player animation controller for jumping and the transitions from each state. Also removed sticking from walls so player is no longer able to hold A or D to remain on walls after jumping.

--- Assets/Player/PlayerMovement/PlayerMovement.cs
+++ Assets/Player/PlayerMovement/PlayerMovement.cs
@@ -26,29 +26,40 @@ public class PlayerMovement : MonoBehaviour {
     public LayerMask whatIsGround;//checks which layer is currently considered the ground
     public float gravityForce = -40f;
 
-    //checks if player is next to wall (if player is next to wall disable horizontal input (x-axis))
-    bool wallNear = false;//not next to a wall on right side
-    public Transform wallCheck;//transform at player sides to check if they are close to a wall
-    float wallRadius = 1f;//how large the circle is when checking distance to wall
-    public LayerMask whatIsWall;//checks which layer is currently considered a wall
+    //checks if the player is close to a wall on either the left or right side
+    //if the player is close to the wall, stop x movement
+    //bool wallLeft = false;//not near left wall
+    //bool wallRight = false;//not near right wall
+    //public Transform wallCheckLeft;//tansform at left side of player to check if near wall
+    //public Transform wallCheckRight;//tansform at right side of player to check if near wall
+    //float wallRadius = 0.2f;
+    //public LayerMask whatIsWall;//determines what is considered a wall layer (what the player cannot interact with)
 
     //physics in fixed update
     private void FixedUpdate()
     {
-        //true or false output for whether the wall transform hit the whatIsWall with the wallRadius
-        wallNear = Physics2D.OverlapCircle(wallCheck.position, wallRadius, whatIsWall);
+        //true or false output for whether the wall transforms are hitting the whatIsWall with the wallRadius
+        //wallLeft = Physics2D.OverlapCircle(wallCheckLeft.position, wallRadius, whatIsWall);
+        //wallRight = Physics2D.OverlapCircle(wallCheckRight.position, wallRadius, whatIsWall);
 
         //true or false output for whether the ground transform hit the whatIsGround with the groundRadius
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);//tell the animator that the player is grounded
 
         //get movement direction of player
         float move = Input.GetAxis("Horizontal");
+
         //create movement in the player horizontal direction
-        //if ((wallCheck == false) && (Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.D)))
+        //if ((wallLeft) && (Input.GetKeyDown(KeyCode.A)))//if player is near a wall, unable to move to that direction
         //{
             GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            anim.SetFloat("Speed", Mathf.Abs(move));
         //}
+        //else
+        //{
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        //}
+        
+        anim.SetFloat("Speed", Mathf.Abs(move));
 
         //if facing negative direction and not facing the right FLIP
         if (move > 0 && !faceRight)
@@ -63,7 +74,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Update()
     {
-        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        if(grounded && Input.GetKeyDown(KeyCode.Space))//determines whether or not the player is near the ground and can jump or not
         {
             //adding jumpforce to the y-axis of the rigidBody
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
