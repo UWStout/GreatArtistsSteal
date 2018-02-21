1ba606a522baefa962e47bef31a042563633076e
Adam Delo <deloa6133@my.uwstout.edu>
2018-02-21 11:16:21 -0600

    Guard movement

--- Assets/Guard/GuardMovement/GuardMovement.cs
+++ Assets/Guard/GuardMovement/GuardMovement.cs
@@ -8,32 +8,50 @@ public class GuardMovement : MonoBehaviour {
     bool faceRight = true;
 
     //left and right wall check
-    bool wallCheck = false;
+    bool wallCheckL = false;
+    bool wallCheckR = false;
     public LayerMask whatIsGuardWall;//determines what is considered a wall for the guard
-    float wallRadius = 1.6f;
-    public Transform wallNear;
-    bool wallNum = false;
+    float wallRadius = 0.2f;
+    public Transform wallNearL;
+    public Transform wallNearR;
+    bool movingLeft = true;
+
 
     //guard move speed
     public float walkSpeed = 3f;
 
     private void FixedUpdate()
     {
-        wallCheck = Physics2D.OverlapCircle(wallNear.position, wallRadius, whatIsGuardWall);
+        wallCheckL = Physics2D.OverlapCircle(wallNearR.position, wallRadius, whatIsGuardWall);
+        wallCheckR = Physics2D.OverlapCircle(wallNearL.position, wallRadius, whatIsGuardWall);
+
+        switch (movingLeft)
+        {
+            case true:
+                moveLeft();
+                if (wallCheckL)
+                {
+                    movingLeft = false;
+                    Debug.Log("Left Wall Check");
+                }
+                break;
+
+            case false:
+                moveRight();
+                if (wallCheckR)
+                {
+                    movingLeft = true;
+                    Debug.Log("Right Wall Check");
+                }
+                break;
+        }
     }
 
     private void Update()
     {
-        if (wallCheck == false && wallNum == false)
-        {
-            moveLeft();
-        }
-        else if ((wallCheck == true))
-        {
-            moveRight();
-            wallNum = true;
-        }
+
         
+
     }
 
     public void moveLeft()
