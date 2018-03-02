415a53f02e63ba01da50a9a91fcdc215192f7f6d
deloa6133 <36006013+deloa6133@users.noreply.github.com>
2018-03-02 13:31:18 -0600

    added camera follow horizontal only (will be modified)

---
+++ Assets/Camera/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 position = transform.position;
+        position.x = player.transform.position.x;
+        position.y = 4;
+        transform.position = position;
+	}
+}
