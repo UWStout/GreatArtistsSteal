22556ac108bc4da8e0494389239732b7d57fe1a5
Adam Delo <deloa6133@my.uwstout.edu>
2018-03-21 10:48:15 -0500

    stealable prefab

--- Assets/Stealables/Steal.cs
+++ Assets/Stealables/Steal.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class Steal : MonoBehaviour {
 
+    private bool canSteal = false;
+
+    private void Update()
+    {
+        if (canSteal == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("10$");
+                Destroy(gameObject);
+
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (gameObject.transform.tag == ("Player"))
+        if (collision.transform.tag == ("Player"))
         {
+            canSteal = true;
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == ("Player"))
+        {
+            canSteal = false;
         }
     }
 }
