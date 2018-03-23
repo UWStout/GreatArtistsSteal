f153961b3b1321fae0d228bae256e09803c20858
Aaron Gary <garya2365@my.uwstout.edu>
2018-03-23 10:55:47 -0500

    Removing an error

--- Assets/LevelGenScripts/LevelOneGen.cs
+++ Assets/LevelGenScripts/LevelOneGen.cs
@@ -185,7 +185,7 @@ for (int x = maxX-1; x >= 0; x--)
 
 // Finding the index of the treasure Room and
 // making sure it is not supposed to be an empty Room or stair room
-/**/
+/*
 int randY = Random.Range(0,9);
 int randX = Random.Range(5,6);
 if(level[randX, randY] != 1 ||  level[randX,randY] != 5 ||  level[randX,randY] != 2  ){
@@ -198,7 +198,7 @@ if(level[randX, randY] != 1 ||  level[randX,randY] != 5 ||  level[randX,randY] !
         }
     }
 }
-// */
+ */
 
 
 //*Placing The Rooms */
