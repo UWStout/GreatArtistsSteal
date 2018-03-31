ddbeb5340805a9ef99f2c39aa46303379cb0f5a0
Noah Wussow <noahwus@gmail.com>
2018-03-31 16:37:32 -0500

    Working on Cloud Spawning + Paralax

---
+++ Assets/Background/PreFabs/CloudSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawn : MonoBehaviour {
+	public float dist = 0;
+	// Use this for initialization
+	void Start () {
+		dist = Random.Range (8f, 15f);
+		Vector3 position = new Vector3(Random.Range(-220.0f, 200.0f), Random.Range(0f, 71f), dist);
+		transform.SetPositionAndRotation(position,transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate ((dist / 8) * Time.deltaTime, 0, 0);
+
+		if (transform.position.x > 215) {
+			dist = Random.Range (8f, 15f);
+			Vector3 position = new Vector3(Random.Range(-220.0f, -230.0f), Random.Range(0f, 71f), dist);
+			transform.SetPositionAndRotation(position,transform.rotation);
+		}
+	}
+}
---
+++ Assets/Background/PreFabs/MoonSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSpawn : MonoBehaviour {
+	public GameObject cloud1;
+	public GameObject cloud2;
+	public GameObject cloud3;
+	public GameObject cloud4;
+	public GameObject cloud5;
+
+	// Use this for initialization
+	void Start () {
+		int temp = 0;
+		int low = 1;
+		int high = 8;
+		temp = Random.Range (low, high);
+		for(int i=0; i<temp; i++){
+			Instantiate(cloud1, transform.position, transform.rotation);
+		}
+		temp = Random.Range (low, high);
+		for(int i=0; i<temp; i++){
+			Instantiate(cloud2, transform.position, transform.rotation);
+		}
+		temp = Random.Range (low, high);
+		for(int i=0; i<temp; i++){
+			Instantiate(cloud3, transform.position, transform.rotation);
+		}
+		temp = Random.Range (low, high);
+		for(int i=0; i<temp; i++){
+			Instantiate(cloud4, transform.position, transform.rotation);
+		}
+		temp = Random.Range (low, high);
+		for(int i=0; i<temp; i++){
+			Instantiate(cloud5, transform.position, transform.rotation);
+		}
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
