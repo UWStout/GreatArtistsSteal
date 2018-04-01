81053a69fa1ba8ab71b25d7f236078af13a49c34
Noah Wussow <noahwus@gmail.com>
2018-04-01 13:43:28 -0500

    Background Mountains, Moon movement

--- Assets/Background/PreFabs/MoonSpawn.cs
+++ Assets/Background/PreFabs/MoonSpawn.cs
@@ -9,11 +9,14 @@ public class MoonSpawn : MonoBehaviour {
 	public GameObject cloud4;
 	public GameObject cloud5;
 
+	public float speed = 0.1f;
+
 	// Use this for initialization
 	void Start () {
+		
 		int temp = 0;
 		int low = 1;
-		int high = 8;
+		int high = 10;
 		temp = Random.Range (low, high);
 		for(int i=0; i<temp; i++){
 			Instantiate(cloud1, transform.position, transform.rotation);
@@ -41,6 +44,6 @@ public class MoonSpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate (speed* Time.deltaTime, 0f, 0f);
 	}
 }
