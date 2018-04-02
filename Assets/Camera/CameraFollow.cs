0257e49c1519d53e76b413264b6180e8b227fadf
deloa6133 <36006013+deloa6133@users.noreply.github.com>
2018-04-02 17:30:42 -0500

    added camera smoothing (can remove if wanted)

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -13,10 +13,15 @@ public class CameraFollow : MonoBehaviour {
     public Transform playerObject;
     public Transform guardObject;
 
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 80f;
     //public Vector3 offset;
 
     public Transform camera;
+
+    private void Start()
+    {
+        transform.position = new Vector3(player.position.x, cameraHeight, -13);
+    }
 
     // Update is called once per frame
     void Update () {
@@ -26,6 +31,8 @@ public class CameraFollow : MonoBehaviour {
         //currently unused_______________________________________________________
         //Vector3 smoothedPosition = Vector3.Lerp(position, position, smoothSpeed);
         //_______________________________________________________________________
+
+        float xPos = Mathf.Lerp(transform.position.x, player.transform.position.x, 50 * smoothSpeed * Time.deltaTime);
 
         if (player.position.x <= 4)
         {
@@ -39,9 +46,10 @@ public class CameraFollow : MonoBehaviour {
         }
         else
         {
-            position.x = player.transform.position.x;
+            /*position.x = player.transform.position.x;
             position.y = cameraHeight;
-            transform.position = position;
+            transform.position = position;*/
+            transform.position = new Vector3(xPos, cameraHeight, -13);
         }
 
         /*position.x = player.transform.position.x;
