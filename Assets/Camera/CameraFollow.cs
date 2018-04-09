fbbfe42e6d335bcc8d95764362d4733d7ba48653
Adam Delo <deloa6133@my.uwstout.edu>
2018-04-09 10:21:39 -0500

    removed camera pull to enemy

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -20,25 +20,13 @@ public class CameraFollow : MonoBehaviour {
     //public Vector3 offset;
 
     public Transform camera;
-    private bool pull = false;
+    //private bool pull = false;
 
 
     private void Start()
     {
         guard = GameObject.FindGameObjectsWithTag("Guard");
         transform.position = new Vector3(player.position.x, cameraHeight, -13);
-    }
-
-    private void FixedUpdate()
-    {
-        if (distance < distanceToGuard)
-        {
-            pull = true;
-        }
-        else if (distance > distanceToGuard)
-        {
-            pull = false;
-        }
     }
 
     // Update is called once per frame
@@ -57,62 +45,23 @@ public class CameraFollow : MonoBehaviour {
         float xPos3 = Mathf.Lerp(transform.position.x, player.transform.position.x - 1.5f, 50 * smoothSpeed * Time.deltaTime);
 
 
-        if ((player.position.x <= 3) && (pull == false))
+        if ((player.position.x <= 3))
         {
-            position.x = 4;
+            position.x = 3;
             position.y = cameraHeight;
         }
-        else if ((player.position.x <= 3) && (pull == true))
+        else if ((player.position.x >= 221))
         {
-            position.x = 4;
+            position.x = 221;
             position.y = cameraHeight;
         }
-        else if ((player.position.x >= 220) && (pull = false))
-        {
-            position.x = 220;
-            position.y = cameraHeight;
-        }
-        else if ((player.position.x >= 220) && (pull == true))
-        {
-            position.x = 4;
-            position.y = cameraHeight;
-        }
-        else if (((player.position.x > 3) || (player.position.x < 220)) && (pull == false))
+        else if (((player.position.x > 3) || (player.position.x < 220)))
         {
             /*position.x = player.transform.position.x;
             position.y = cameraHeight;
             transform.position = position;*/
             transform.position = new Vector3(xPos, cameraHeight, -13);
         }
-        else if (((player.position.x > 3) || (player.position.x < 220)) && (pull == true))
-        {
-            if (player.position.x < guardObject.position.x)
-            {
-                transform.position = new Vector3(xPos2, cameraHeight, -13);
-            }
-            else if (player.position.x > guardObject.position.x)
-            {
-                transform.position = new Vector3(xPos3, cameraHeight, -13);
-            }
-        }
-            
-        /*if (player.position.x - guardObject.position.x < distanceToGuard)
-        {
-            if (player.position.x < guardObject.position.x)
-            {
-                transform.position = new Vector3(xPos2, cameraHeight, -13);
-                pull = true;
-            }
-            else if (player.position.x > guardObject.position.x)
-            {
-                transform.position = new Vector3(xPos3, cameraHeight, -13);
-                pull = true;
-            }
-        }
-        else
-        {
-            pull = false;
-        }*/
 
     }
 }
