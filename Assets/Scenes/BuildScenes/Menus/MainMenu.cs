428c817196202e35521af1805850778072cce132
Noah Wussow <noahwus@gmail.com>
2018-04-19 18:21:52 -0500

    Main Menu Push  /  .meta gitignore (IMPORTANT?)

---
+++ Assets/Scenes/BuildScenes/Menus/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Start(){
+
+		SceneManager.LoadScene("Level One");
+
+	}
+
+	public void Credits(){
+
+	}
+
+	public void Highscore(){
+
+	}
+
+	public void Quit(){
+
+	}
+}
