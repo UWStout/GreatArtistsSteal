07bff00904c1ba2724556df4022736e7681f12d1
Adam Delo <deloa6133@my.uwstout.edu>
2018-04-20 11:35:31 -0500

    changed GUI

--- Assets/GUI/GUI_HasKey.cs
+++ Assets/GUI/GUI_HasKey.cs
@@ -13,13 +13,13 @@ public class GUI_HasKey : MonoBehaviour {
 	void Update () {
 		if (Key.hasKey == true)
         {
-            gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            //gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
         }
         else if (Key.hasKey == false)
         {
-            gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
         }
 	}
 }
