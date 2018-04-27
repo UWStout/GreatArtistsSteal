518b7d53f0493dffa48a66cbdd596b6a5add86b1
Adam Delo <deloa6133@my.uwstout.edu>
2018-04-27 11:46:41 -0500

    pause menu, continue button working

---
+++ Assets/Main Menu/PauseMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
