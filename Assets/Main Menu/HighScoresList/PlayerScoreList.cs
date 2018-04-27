b352d0f084cd972d4e737730de1e06a631eefcec
Noah Wussow <noahwus@gmail.com>
2018-04-27 14:16:33 -0500

    Level Briefing 2-5 ; Work on highscores list

---
+++ Assets/Main Menu/HighScoresList/PlayerScoreList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreList : MonoBehaviour {
+
+	public GameObject playerScoreEntryPrefab;
+
+	ScoreBoard scoreBoard;
+
+	// Use this for initialization
+	void Start () {
+		scoreBoard = GameObject.FindObjectOfType<ScoreBoard> ();
+
+		if (scoreBoard == null) {
+			Debug.LogError ("you for got to add the score board component");
+			return;
+		}
+
+		string[] names = ScoreBoard.getPlayerNames();
+
+		for (int i = 0; i < 5; i++) {
+			GameObject go = (GameObject)Instantiate (playerScoreEntryPrefab);
+			go.transform.SetParent (this.transform);
+			go.transform.Find ("PlayerName").GetComponent<Text>().text = name;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Main Menu/HighScoresList/ScoreBoard.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScoreBoard : MonoBehaviour {
+
+	//List of "Users"
+	Dictionary<string, Dictionary<string, int>> playerScores;
+
+	void Start(){
+		setScore ("quill18", "Time / Level", 9001);
+		setScore ("quill18", "Money", 9001);
+
+		setScore ("penis ", "Time / Level", 2201);
+		setScore ("penis", "Money", 901);
+
+		setScore ("assHAT", "Time / Level", 3001);
+		setScore ("assHAT", "Money", 69);
+
+
+
+
+		Debug.Log( getScore("quill18", "kills") );
+	}
+
+	void Init(){
+		if (playerScores != null) {
+			return;
+		}
+
+		playerScores = new Dictionary<string, Dictionary<string, int>>();
+
+	}
+
+	public int getScore(string username, string scoreType){
+		Init ();
+
+		if (playerScores.ContainsKey (username) == false) {
+			return 0;
+		}
+
+		if (playerScores [username].ContainsKey (scoreType) == false) {
+			return 0;
+		}
+
+		return playerScores [username] [scoreType];
+	}
+
+	public void setScore(string username, string scoreType, int value){
+		Init ();
+
+		if (playerScores.ContainsKey (username) == false) {
+			
+			playerScores[username] = new Dictionary<string, int>();
+		}
+
+		if (playerScores [username].ContainsKey (scoreType) == false) {
+			//return 0;
+		}
+
+		//return playerScores [username] [scoreType];
+
+	}
+
+	public void changeScore(string username, string scoreType, int amount){
+		Init ();
+		int curScore = getScore (username, scoreType);
+		setScore (username, scoreType, curScore + amount);
+	}
+
+	public string[] getPlayerNames(){
+		Init ();
+		return playerScores.Keys.ToArray();
+	}
+
+}
