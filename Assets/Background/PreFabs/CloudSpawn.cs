14079bdfaccb3c3b96e425387596c63cd0894b4e
Noah Wussow <noahwus@gmail.com>
2018-04-29 17:08:22 -0500

    Paralax background work ~ not going to get to the level I would have liked

--- Assets/Background/PreFabs/CloudSpawn.cs
+++ Assets/Background/PreFabs/CloudSpawn.cs
@@ -7,7 +7,7 @@ public class CloudSpawn : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		dist = Random.Range (8f, 15f);
-		Vector3 position = new Vector3(Random.Range(-220.0f, 200.0f), Random.Range(0f, 71f), dist);
+		Vector3 position = new Vector3(Random.Range(40, 340.0f), Random.Range(0f, 61f), dist);
 		transform.SetPositionAndRotation(position,transform.rotation);
 	}
 	
@@ -15,9 +15,9 @@ public class CloudSpawn : MonoBehaviour {
 	void Update () {
 		transform.Translate ((dist / 8) * Time.deltaTime, 0, 0);
 
-		if (transform.position.x > 215) {
+		if (transform.position.x > 375) {
 			dist = Random.Range (8f, 15f);
-			Vector3 position = new Vector3(Random.Range(-220.0f, -230.0f), Random.Range(0f, 71f), dist);
+			Vector3 position = new Vector3(Random.Range(-40.0f, -10.0f), Random.Range(10f, 51f), dist);
 			transform.SetPositionAndRotation(position,transform.rotation);
 		}
 	}
