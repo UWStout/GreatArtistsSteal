d7e9994fb175c69c4dfa20eca44a1cee8b0de9af
Noah Wussow <noahwus@gmail.com>
2018-04-30 11:47:13 -0500

    Splash

---
+++ Assets/Main Menu/LevelBreifing/briefBounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class briefBounce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
